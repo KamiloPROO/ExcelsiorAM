21f0196b9cf6dc6a005579c8ab41e7dcfc657dd9
AndresWnj <andres.wnj@gmail.com>
2022-10-27 10:54:40 -0500

    welcome

--- app/src/main/java/ammovil/com/excelsior/ui/Dashboard/DashboarFragment.java
+++ app/src/main/java/ammovil/com/excelsior/ui/Dashboard/DashboarFragment.java
@@ -11,23 +11,45 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
+import ammovil.com.excelsior.R;
 import ammovil.com.excelsior.databinding.FragmentDashboardBinding;
 
 public class DashboarFragment extends Fragment {
 
+    SliderView sliderView;
+    int[] images = {R.drawable.one,
+            R.drawable.two,
+            R.drawable.three,
+            R.drawable.four,
+            R.drawable.five,
+            R.drawable.six};
+
+
     private FragmentDashboardBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         DashboardViewModel galleryViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        sliderView = binding.WelcomeSlider;
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
+
         final TextView textView = binding.textDashboard;
         galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+
+
     }
 
     @Override
